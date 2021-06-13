2330d652c9beac125bd208423500523504c72c7d
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2021-06-13 17:26:03 +0200

    renew ui

---
+++ schattenzorn-ui/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test"> hello</div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.test {
+  background-color: black;
+}
+</style>
