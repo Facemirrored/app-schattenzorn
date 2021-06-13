b510148dfd55f7d2557a678db0097f32a555b392
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2021-06-13 13:39:24 +0200

    Init Spring Boot / Maven

---
+++ src/main/java/de/facemirrored/appschattenzorn/AppSchattenzornApplication.java
@@ -0,0 +1,13 @@
+package de.facemirrored.appschattenzorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSchattenzornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSchattenzornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/facemirrored/appschattenzorn/AppSchattenzornApplicationTests.java
@@ -0,0 +1,13 @@
+package de.facemirrored.appschattenzorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSchattenzornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
