3255ea0d862b18812a4dff1e9bbf56c9fdb1ec09
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2021-06-13 18:33:41 +0200

    add Lorem Impsum

--- schattenzorn-ui/src/App.vue
+++ schattenzorn-ui/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <navbar></navbar>
+  <home></home>
 </template>
 
 <script>
 
 import Navbar from "@/components/Navbar";
+import Home from "@/views/Home";
 
 export default {
   name: 'App',
   components: {
     navbar: Navbar,
+    home: Home,
   }
 }
 </script>
@@ -24,6 +27,6 @@ export default {
 
 body {
   background: rgb(0,0,0);
-  background: linear-gradient(120deg, rgba(0,0,0,1) 20%, rgba(255,151,0,1) 40%, rgba(0,0,0,1) 60%);
+  background: linear-gradient(120deg, rgba(0,0,0,1) 20%, rgba(255, 151, 0, 0.93) 40%, rgba(0,0,0,1) 60%);
 }
 </style>
---
+++ schattenzorn-ui/src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container welcome-container mt-5">
+    <h1>Schattenzorn</h1>
+    <p>Willkommen auf dieser noch sehr stumpfen Homepage!</p>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+      kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+      duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+      Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
+      feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+      delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,</p>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+.welcome-container {
+
+  background: rgba(232, 232, 232, 1);
+  border-radius: 10px;
+  color: black;
+}
+
+h1 {
+  color: #111;
+  font-family: 'Helvetica Neue', sans-serif;
+  font-size: 100px;
+  font-weight: bold;
+  letter-spacing: -1px;
+  line-height: 1;
+  text-align: center;
+}
+
+
+h2 {
+  color: #111;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 30px;
+  font-weight: 300;
+  line-height: 32px;
+  margin: 0 0 72px;
+  text-align: center;
+}
+
+
+p {
+  color: #685206;
+  font-family: 'Helvetica Neue', sans-serif;
+  font-size: 14px;
+  line-height: 24px;
+  margin: 0 0 24px;
+  text-align: justify;
+  text-justify: inter-word;
+}
+
+</style>
