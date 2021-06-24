306e761f6eb718be40d924062b00c59e278b1b36
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2021-06-24 18:41:19 +0200

    Fix wide screen horizontal line

--- schattenzorn-ui/src/components/NewsSlot.vue
+++ schattenzorn-ui/src/components/NewsSlot.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="slot-container">
-    <div class="row mt-3 mb-md-4 mb-lg-4">
+    <div class="row mt-3 mb-md-4 mb-lg-5">
       <div class="col-12 col-md-5 col-lg-4 position-relative">
         <div class="row">
           <div class="col-3 col-sm-2 col-md-4">
