742690c4f1e931a8d740c2fd3b91275ea9271e8e
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2021-06-29 12:58:00 +0200

    Change router to hash mode

--- schattenzorn-ui/src/router/router.ts
+++ schattenzorn-ui/src/router/router.ts
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Rules from "@/views/Rules.vue";
@@ -40,7 +44,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
